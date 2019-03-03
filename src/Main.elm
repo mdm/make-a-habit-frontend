5b72600b3a3bcc8eb66daa31afb6a0538cb88db3
Marc Dominik Migge <marcmigge@gmx.net>
2019-03-03 18:19:37 +0100

    Add nav bar

--- src/Main.elm
+++ src/Main.elm
@@ -3,7 +3,12 @@ module Main exposing (main)
 import Browser
 import Browser.Navigation as Nav
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Url
+
+import Bootstrap.CDN as CDN
+import Bootstrap.Navbar as Navbar
+import Bootstrap.Grid as Grid
 
 
 -- MODEL
@@ -11,17 +16,23 @@ import Url
 type alias Model =
   { url : Url.Url
   , key : Nav.Key
+  , navbarState : Navbar.State
   }
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-  ( Model url key, Cmd.none )
+  let
+    ( navbarState, navbarCmd ) =
+      Navbar.initialState GotNavbarMsg
+  in
+  ( Model url key navbarState, navbarCmd )
 
 -- UPDATE
 
 type Msg
   = ClickedLink Browser.UrlRequest
   | ChangedUrl Url.Url
+  | GotNavbarMsg Navbar.State
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -34,16 +45,33 @@ update msg model =
           ( model, Nav.load href )
     ChangedUrl url ->
       ( { model | url = url }, Cmd.none )
+    GotNavbarMsg state ->
+      ( { model | navbarState = state }, Cmd.none )
 
 -- VIEW
 
 view : Model -> Browser.Document Msg
 view model =
   { title = "Make A Habit"
-  , body =
-    [ h1 [] [ text "Hello world!" ]
-    ]
+  , body = CDN.stylesheet :: viewHeader model :: viewContent :: [viewFooter]
   }
+
+viewHeader : Model -> Html Msg
+viewHeader model =
+  Navbar.config GotNavbarMsg
+  --|> Navbar.withAnimation
+  |> Navbar.brand [ href "#" ] [ text "Brand" ]
+  |> Navbar.items
+    [ Navbar.itemLink [ href "#" ] [ text "Item 1" ]
+    , Navbar.itemLink [ href "#" ] [ text "Item 2" ]
+    ]
+  |> Navbar.view model.navbarState
+
+viewContent : Html Msg
+viewContent = text ""
+
+viewFooter : Html Msg
+viewFooter = text ""
 
 -- SUBSCRIPTIONS
 
