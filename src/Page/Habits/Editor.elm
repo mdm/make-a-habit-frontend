fc74a1fea6f550064da92d104bf4467b231617de
Marc Dominik Migge <marcmigge@gmx.net>
2019-08-11 15:19:54 +0200

    Use model to populate habit editor

--- src/Page/Habits/Editor.elm
+++ src/Page/Habits/Editor.elm
@@ -1,7 +1,7 @@
 module Page.Habits.Editor exposing (..)
 
-import Html exposing (Html, a, button, dd, div, dl, dt, form, h3, input, label, main_, option, select, strong, text)
-import Html.Attributes exposing (checked, class, for, id, tabindex, type_, value)
+import Html exposing (Html, Attribute, a, button, dd, div, dl, dt, form, h3, input, label, main_, option, select, strong, text)
+import Html.Attributes exposing (checked, class, for, id, selected, tabindex, type_, value)
 import Route
 import Session exposing (Session)
 import Habit.Id exposing (HabitId)
@@ -33,10 +33,10 @@ initNew session =
     ( { session = session
       , status =
             EditingNew []
-                { name = ""
-                , description = ""
-                , timeLimit = 1
-                , recurrences = []
+                { name = "Test"
+                , description = "Bla bla"
+                , timeLimit = 3
+                , recurrences = [3, 5]
                 }
       }
     , Cmd.none
@@ -61,6 +61,17 @@ view model =
 
                 Nothing ->
                     "New Habit"
+        formHtml =
+            case model.status of
+                Loading _ ->
+                    []
+
+                Editing habitId problems form ->
+                    [ viewForm form problems (editHabitSaveButton []) ]
+
+                EditingNew problems form ->
+                    [ viewForm form problems (newHabitSaveButton []) ]
+
     in
     { title = title
     , content =
@@ -69,59 +80,74 @@ view model =
                 [ div [ class "Box-header" ]
                     [ h3 [ class "Box-title" ] [ text title ] ]
                 , div [ class "Box-body" ]
-                    [ form []
-                        [ dl [ class "form-group" ]
-                            [ dt []
-                                [ label [ for "name" ] [ text "Name" ] ]
-                            , dd []
-                                [ input [ class "form-control", type_ "text", id "name"] [] ]
-                            ]
-                        , dl [ class "form-group" ]
-                            [ dt []
-                                [ label [ for "description" ] [ text "Description" ] ]
-                            , dd []
-                                [ input [ class "form-control", type_ "text", id "description"] [] ]
-                            ]
-                        , dl [ class "form-group" ]
-                            [ dt []
-                                [ label [ for "time-limit" ] [ text "Time limit for completion" ] ]
-                            , dd []
-                                [ select [ class "form-select", id "time-limit"]
-                                    (List.range 1 7 |> List.map viewTimeLimit)
-                                ]
-                            ]
-                        , div [] <|
-                            strong [] [ text "Repeats every week on" ]
-                            :: List.map viewRecurrence [ "Monday", "Tueday", "Wednesday", "Thursday", "Friday", "Satuday", "Sunday" ]
-                        , div [ class "form-actions" ]
-                            [ button [ class "btn btn-primary", type_ "submit" ] [ text "Submit" ]
-                            , a [ class "btn", Route.href Route.Habits] [ text "Cancel" ]
-                            ]
-                        ]
-                    ]
+                    formHtml
                 , div [ class "Box-footer text-right" ] []
                 ]
             ]
     }
 
-viewTimeLimit : Int -> Html Msg
-viewTimeLimit days =
+viewForm : Form -> List Problem -> Html Msg -> Html Msg
+viewForm fields problems saveButton =
+    form []
+        [ dl [ class "form-group" ]
+            [ dt []
+                [ label [ for "name" ] [ text "Name" ] ]
+            , dd []
+                [ input [ class "form-control", type_ "text", id "name", value fields.name ] [] ]
+            ]
+        , dl [ class "form-group" ]
+            [ dt []
+                [ label [ for "description" ] [ text "Description" ] ]
+            , dd []
+                [ input [ class "form-control", type_ "text", id "description", value fields.description ] [] ]
+            ]
+        , dl [ class "form-group" ]
+            [ dt []
+                [ label [ for "time-limit" ] [ text "Time limit for completion" ] ]
+            , dd []
+                [ select [ class "form-select", id "time-limit"]
+                    (List.range 1 7 |> List.map (viewTimeLimit fields.timeLimit))
+                ]
+            ]
+        , div [] <|
+            strong [] [ text "Repeats every week on" ]
+            :: List.indexedMap (viewRecurrence fields.recurrences) [ "Monday", "Tueday", "Wednesday", "Thursday", "Friday", "Satuday", "Sunday" ]
+        , div [ class "form-actions" ]
+            [ saveButton
+            , a [ class "btn", Route.href Route.Habits] [ text "Cancel" ]
+            ]
+        ]
+
+viewTimeLimit : Int -> Int -> Html Msg
+viewTimeLimit selectedTimeLimit days =
     let
         formattedDays =
             case days of
                 1 -> String.fromInt days ++ " day"
                 _ -> String.fromInt days ++ " days"
     in
-    option [ value formattedDays ] [ text formattedDays ]
+    option [ value <| String.fromInt days, selected <| days == selectedTimeLimit ] [ text formattedDays ]
 
-viewRecurrence : String -> Html Msg
-viewRecurrence day =
+viewRecurrence : List Int -> Int -> String -> Html Msg
+viewRecurrence checkedRecurrences dayIndex dayName =
     div [ class "form-checkbox" ]
         [ label []
-            [ input [ type_ "checkbox", id <| String.toLower day, checked True ] []
-            , text day
+            [ input [ type_ "checkbox", id <| String.toLower dayName, checked (List.member dayIndex checkedRecurrences) ] []
+            , text dayName
             ]
         ]
+
+newHabitSaveButton : List (Attribute msg) -> Html msg
+newHabitSaveButton extraAttrs =
+    saveHabitButton "Create" extraAttrs
+
+editHabitSaveButton : List (Attribute msg) -> Html msg
+editHabitSaveButton extraAttrs =
+    saveHabitButton "Save" extraAttrs
+
+saveHabitButton : String -> List (Attribute msg) -> Html msg
+saveHabitButton caption extraAttrs =
+    button ([ class "btn btn-primary", type_ "submit" ] ++ extraAttrs) [ text caption ]
 
 
 type Msg
